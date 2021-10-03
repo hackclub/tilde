245c4c66f75468177db0f492284b4cc8956cd2b6
Scott Little <swlittle7@gmail.com>
2021-10-03 16:18:08 -0400

    remove old users

--- users/default.nix
+++ users/default.nix
@@ -4,8 +4,6 @@
   imports = [
 /home/maddie/nixos/users/maddie.nix
 /home/maddie/nixos/users/csalter.nix
-/home/maddie/nixos/users/lin.nix
-/home/maddie/nixos/users/me.nix
 /home/maddie/nixos/users/arc.nix
   ];
 }
