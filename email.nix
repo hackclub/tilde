7087fdf5400acf228e5dca6b5061a254f053a71b
Scott Little <swlittle7@gmail.com>
2021-10-03 17:50:27 -0400

    fix postfix domain

--- email.nix
+++ email.nix
@@ -9,8 +9,8 @@
   services.postfix = {
     enable = true;
     config = {
-      myhostname = "mail.tilde.hackclub.com";
-      mydomain = "tilde.hackclub.com";
+      myhostname = "mail.tilde";
+      mydomain = "hackclub.com";
       mynetworks = "127.0.0.1";
       home_mailbox = "mail/";
       mailbox_command = "";
