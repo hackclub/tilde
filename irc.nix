b77d525d36da2e5d637f5cd56cffbe867ed2e8d5
Scott Little <swlittle7@gmail.com>
2021-11-19 11:50:35 -0600

    Add irc O-Line

--- irc.nix
+++ irc.nix
@@ -6,14 +6,23 @@
   services.inspircd = {
     enable = true;
     config = ''
+    <module name="operjoin">
     <server
-      name="localhost"
+      name="tilde.hackclub.com"
       description="Hack Club local IRC server"
       network="~Hack">
     <admin
-      name="Madeline"
-      nick="maddie"
-      email="hack@club">
+      name="Mirabelle"
+      nick="mirabelle"
+     >
+     <oper
+      name="mirabelle"
+      password="$5$J0mx6gbzgl$Ta/v8xo4y.6WTyQM9y9tGykLen7EoUF2AnSmcEmlMK0"
+      hash="sha256"
+      host="*"
+      type="NetAdmin"
+      autojoin="#lobby,#announcement"
+    >
     <bind
       address="0.0.0.0"
       port="6697"
