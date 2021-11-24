6aafffef65cd630b187f99c752aedd32d3c17b0e
Scott Little <swlittle7@gmail.com>
2021-11-24 18:48:26 +0000

    add right param

--- flake.nix
+++ flake.nix
@@ -33,7 +33,7 @@
 	  sops.secrets.tokens = {
             sopsFile = ./secrets/slack.yaml;
             mode = "0440";
-            owner = 69;
+            owner = "straembot";
           };
 	})
       ];
