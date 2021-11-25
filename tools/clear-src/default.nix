b791ba3e6813b6671e4828658a66cf0a6b1f516e
Scott Little <swlittle7@gmail.com>
2021-11-25 21:06:52 +0000

    change bin name

--- tools/clear-src/default.nix
+++ tools/clear-src/default.nix
@@ -3,10 +3,10 @@ with pkgs.stdenv;
 mkDerivation {
   pname = "clear_src";
   version = "0.0";
-  buildInputs = with pkgs; [ jq nix  ];
+  buildInputs = with pkgs; [ jq nix ];
   src = ./.;
   dontBuild = true;
   installPhase = ''
-  install -Dm755 $src/clear_src.sh $out/clear-source
+  install -Dm755 $src/clear_src.sh $out/clear-src
   '';
 }
