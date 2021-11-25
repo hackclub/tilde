f7f65e19f09b65a833a1ca74c926fb451d7571d4
Scott Little <swlittle7@gmail.com>
2021-11-25 20:57:59 +0000

    use right path

--- tools/default.nix
+++ tools/default.nix
@@ -3,13 +3,13 @@ let
   addusr = (import ./addusr {inherit pkgs;});
   newusersetup = (import ./newusersetup {inherit pkgs;});
   rebuild = (import ./rebuild.nix {inherit pkgs;});
-  clear_src = (import ./clear_src {inherit pkgs;});
+  clear-src = (import ./clear-src {inherit pkgs;});
 in
 {
   environment.systemPackages = [
     addusr
     newusersetup
     rebuild
-    clear_src
+    clear-src
   ];
 }
