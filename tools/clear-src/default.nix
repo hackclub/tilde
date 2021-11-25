a9c56fed17ddfedcfdb93fb27a35868c898ae43e
Scott Little <swlittle7@gmail.com>
2021-11-25 21:45:07 +0000

    use writeShellApplication

--- tools/clear-src/default.nix
+++ tools/clear-src/default.nix
@@ -1,12 +1,3 @@
 { pkgs, ... }:
 with pkgs.stdenv;
-mkDerivation {
-  pname = "clear_src";
-  version = "0.0";
-  buildInputs = with pkgs; [ jq nix ];
-  src = ./.;
-  dontBuild = true;
-  installPhase = ''
-  install -Dm755 $src/clear_src.sh $out/clear-src
-  '';
-}
+writeShellApplication "clear-src" (builtins.readFile ./clear_src.nix) (with pkgs; [ jq nix ])
