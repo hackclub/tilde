89e8ad4a2dc37961585d17d228fc10f2e94bbf16
Scott Little <swlittle7@gmail.com>
2021-11-25 21:52:17 +0000

    quote name

--- tools/clear-src/clear_src.sh
+++ tools/clear-src/clear_src.sh
@@ -1,7 +1,5 @@
 REV="$(nixos-version --json | jq .configurationRevision)"
-PATH=$(nix-instantiate --eval --expr \
+SRC_PATH=$(nix-instantiate --eval --expr \
 	"(builtins.fetchGit { url = \"https://github.com/hackclub/tilde\"; ref=\"main\"; rev=$REV;}).outPath" \
 	| tr -d \")
-echo $PATH
-echo $(which nix-store)
-nix-store --delete $PATH
+nix-store --delete "$SRC_PATH"
