58ba2ade10cd6573c7799bc2c680e6e2eb33af85
Scott Little <swlittle7@gmail.com>
2022-03-01 00:54:27 +0000

    clean out old users

--- users/default.nix
+++ users/default.nix
@@ -1,17 +1,12 @@
 { ... }:
 {
    imports = [
-      ./anirudh.nix
 ./arc.nix
 ./builder.nix
 ./caleb.nix
 ./csalter.nix
-./lin.nix
 ./maddie.nix
-./me.nix
 ./msw.nix
 ./straembot.nix
-./test.nix
-./zfogg.nix
    ];
 }
